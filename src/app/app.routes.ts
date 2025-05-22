d82eb44f506cccbcb622f2e8d568cfde52a976ed
Doug-26 <cjdelacruz267@gmail.com>
2025-05-22 19:11:55 +0800

    Add WeatherComponent and display user profile in the home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from '@auth0/auth0-angular';
+import { WeatherComponent } from './pages/weather/weather.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent},
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'weather', component: WeatherComponent, canActivate: [AuthGuard] },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { AuthService } from '@auth0/auth0-angular';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  city: string = '';
+  user: any;
+
+  constructor(public auth : AuthService, private router : Router) { 
+    // Subscribe to the user data from AuthService
+    this.auth.user$.subscribe(userData => {
+      this.user = userData;
+    });
+
+  }
+
+  getWeather() {
+    // Logic to get weather data for the specified city
+    // This could involve making an API call to a weather service
+    console.log(`Getting weather for ${this.city}`);
+  }
 }
